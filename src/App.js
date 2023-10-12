3ab74170b14d4c8557c9b739f34e94bc72d6329c
Milos Minic <minicm034@gmail.com>
2023-10-13 01:26:55 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=API_KEY'; //API_KEY = Your API Access Key
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState('');
+  const [toCurrency, setToCurrency] = useState(''); 
+  const [exchangeRate, setExchangeRate] = useState(1); 
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data && data.rates) {
+          setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+          setFromCurrency(data.base);
+          setToCurrency('EUR');
+          setExchangeRate(data.rates['EUR']);
+        } else {
+          console.error('Data or rates property is missing in the response:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency && toCurrency) {
+      fetch(`${BASE_URL}&base=${fromCurrency}&symbols=${toCurrency}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data && data.rates) {
+            setExchangeRate(data.rates[toCurrency]);
+          }
+        });
+    }
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
+    </>
   );
 }
 
---
+++ src/README.md
@@ -0,0 +1,19 @@
+# Currency Converter Web App
+
+Welcome to the Currency Converter Web App, a simple yet powerful tool for real-time currency conversion. This web application is built with React and allows users to easily convert between various currencies with up-to-date exchange rates.
+
+## Features
+
+- **Currency Conversion**: Seamlessly convert between different currencies.
+- **Real-Time Exchange Rates**: Get accurate and updated exchange rates.
+- **User-Friendly Interface**: An intuitive and user-friendly design.
+- **Mobile Responsiveness**: Enjoy a responsive design that works on various devices.
+
+## Installation
+
+To run this application locally, follow these steps:
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/yourusername/currency-converter-react.git
